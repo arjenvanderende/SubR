770ee8b495cb93f377dbed47b3ef7484b387c029
Arjen van der Ende <arjen.vanderende@gmail.com>
2013-01-23 12:43:17 +0100

    Implemented method for retrieving mkv-files in a directory

--- src/main.rb
+++ src/main.rb
@@ -1,3 +1,11 @@
 #!/usr/bin/env ruby
 
-puts 'Hello World!'
+# Enumerate the video files in a directory
+def get_video_files path
+	Dir.foreach(path).map{|f| f =~ /\.mkv$/ ? f : nil}.compact
+end
+
+# Execute the script
+puts 'SubR - Bulk Subtitle Downloader for TV-series'
+@video_files = get_video_files('/Volumes/Multimedia/Video/TV-series/Flashpoint/season 4')
+puts @video_files
