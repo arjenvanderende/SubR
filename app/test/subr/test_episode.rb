3106e7fcd74bd44586d578e12c0445e26bafd5f5
Arjen van der Ende <arjen.vanderende@gmail.com>
2013-01-31 16:44:05 +0100

    Added episode tests

--- app/test/subr/test_episode.rb
+++ app/test/subr/test_episode.rb
@@ -2,7 +2,18 @@ require 'test/unit'
 require 'subr/episode'
 
 class EpisodeTest < Test::Unit::TestCase
-	def test_name
-		assert_equal 0, 1
+	def test_parse_finds_episode_title
+		episode = SubR::Episode.parse 'S00E00 - Episode name.mkv'
+		assert_equal 'Episode name', episode.title
+	end
+
+	def test_parse_finds_season
+		episode = SubR::Episode.parse 'S01E00 - Episode name.mkv'
+		assert_equal 1, episode.season
+	end
+
+	def test_parse_finds_episode_number
+		episode = SubR::Episode.parse 'S00E01 - Episode name.mkv'
+		assert_equal 1, episode.episode
 	end
 end
